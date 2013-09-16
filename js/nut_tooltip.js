1a3feb837c6f425940f645d041aadb2b99447db3
Zhijian Xia <isword.xia@gmail.com>
2013-09-16 22:24:40 +0800

    Add initial js file.

---
+++ js/nut_tooltip.js
@@ -0,0 +1,10 @@
+/*
+ * This plugin is just used to show some kind of tips
+ * This should be light weight and easy to use
+ * Initial Date: 2013-09-16
+ * Author: Zhijian Xia
+ */
+
+
+//This plugin is based on jQuer(mainly for 1.9.1 and above), this should be confirmed later
+
